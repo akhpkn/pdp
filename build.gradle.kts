3ee937fc443b3ddc8cae2a6df0b4f049e332476c
akhpkn <akh.andr@mail.ru>
2022-05-16 01:43:11 +0300

    commented shadow plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     kotlin("jvm") version "1.6.10"
     kotlin("plugin.spring") version "1.6.10"
     kotlin("kapt") version "1.6.10"
-    id("com.github.johnrengelman.shadow") version "7.1.2"
+//    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 group = "com.github.akhpkn"
